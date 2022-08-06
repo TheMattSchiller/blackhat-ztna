4f2d8e5e9c5d34917e69510abef302ef144a727d
Matt Schiller <mschiller@banyansecurity.io>
2022-08-06 15:41:17 -0700

    install unzip

--- 3-controller/controller.tf
+++ 3-controller/controller.tf
@@ -35,8 +35,7 @@ resource "aws_instance" "controller" {
 
   provisioner "remote-exec" {
     inline = [
-      "sudo apt-get update -y",
-      "sudo apt install -y zip unzip",
+      "sudo apt-get update -y && sudo apt install -y zip unzip",
       "curl -L https://releases.hashicorp.com/boundary/0.9.1/boundary_0.9.1_linux_amd64.zip --output boundary.zip",
       "unzip boundary.zip",
       "mv boundary /tmp/boundary",
