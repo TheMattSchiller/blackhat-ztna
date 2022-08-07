b3779998e1044c0b4e0024891a3a12bb299dd5bb
Matt Schiller <mschiller@banyansecurity.io>
2022-08-07 14:23:18 -0700

    configure boundary

--- 4-worker/worker.tf
+++ 4-worker/worker.tf
@@ -111,6 +111,15 @@ resource "aws_security_group_rule" "allow_web_worker" {
   security_group_id = aws_security_group.worker.id
 }
 
+resource "aws_security_group_rule" "allow_web_worker" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.worker.id
+}
+
 resource "aws_security_group_rule" "allow_9202_worker" {
   type              = "ingress"
   from_port         = 9202
