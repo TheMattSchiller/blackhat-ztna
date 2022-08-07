63fa94932fefb359b784dd292300c01a86e6698f
Matt Schiller <mschiller@banyansecurity.io>
2022-08-07 09:57:17 -0700

    configure boundary

--- 6-users/users.tf
+++ 6-users/users.tf
@@ -8,53 +8,30 @@ kms "awskms" {
 EOT
 }
 
-resource "boundary_user" "backend" {
-  for_each    = var.backend_team
-  name        = each.key
-  description = "Backend user: ${each.key}"
-  account_ids = [boundary_account.backend_user_acct[each.value].id]
+resource "boundary_account_password" "standarduser" {
+  auth_method_id = var.auth_method
+  type           = "password"
+  login_name     = "jeff"
+  password       = "$uper$ecure"
+}
+
+resource "boundary_user" "standarduser" {
+  name        = "standarduser"
+  description = "user with typical permissions"
+  account_ids = [boundary_account_password.standarduser.id]
   scope_id    = var.scope_id
 }
 
-resource "boundary_user" "frontend" {
-  for_each    = var.frontend_team
-  name        = each.key
-  description = "Frontend user: ${each.key}"
-  account_ids = [boundary_account.frontend_user_acct[each.value].id]
+resource "boundary_account_password" "adminuser" {
+  auth_method_id = var.auth_method
+  type           = "password"
+  login_name     = "jeff"
+  password       = "$uper$ecure"
+}
+
+resource "boundary_user" "adminuser" {
+  name        = "adminuser"
+  description = "user with typical permissions"
+  account_ids = [boundary_account_password.adminuser.id]
   scope_id    = var.scope_id
-}
-
-resource "boundary_account" "backend_user_acct" {
-  for_each       = var.backend_team
-  name           = each.key
-  description    = "User account for ${each.key}"
-  type           = "password"
-  login_name     = lower(each.key)
-  password       = "foofoofoo"
-  auth_method_id = var.auth_method
-}
-
-resource "boundary_account" "frontend_user_acct" {
-  for_each       = var.frontend_team
-  name           = each.key
-  description    = "User account for ${each.key}"
-  type           = "password"
-  login_name     = lower(each.key)
-  auth_method_id = var.auth_method
-}
-
-resource "boundary_account_password" "backend" {
-  for_each       = var.backend_team
-  auth_method_id = var.auth_method
-  type           = "password"
-  login_name     = each.key
-  password       = "foofoofoo"
-}
-
-resource "boundary_account_password" "frontend" {
-  for_each       = var.frontend_team
-  auth_method_id = var.auth_method
-  type           = "password"
-  login_name     = each.key
-  password       = "foofoofoo"
 }
