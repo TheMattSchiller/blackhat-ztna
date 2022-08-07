65710b786505196a65cea99c604b4af200b94db3
Matt Schiller <mschiller@banyansecurity.io>
2022-08-07 15:01:36 -0700

    configure boundary

--- 10-ssh-target/target.tf
+++ 10-ssh-target/target.tf
@@ -15,16 +15,7 @@ resource "boundary_target" "ssh-target" {
   scope_id                 = var.org_scope
   session_connection_limit = -1
   default_port             = 22
-  host_source_ids = [boundary_host_set_static.target_ssh.id]
-}
-
-resource "boundary_host_set_static" "target_ssh" {
-  name            = "target_ssh"
-  host_catalog_id = var.host_catalog_id
-
-  host_ids = [
-    boundary_host_static.target_ssh.id,
-  ]
+  host_source_ids = [boundary_host_static.target_ssh.id]
 }
 
 resource "boundary_host_static" "target_ssh" {
