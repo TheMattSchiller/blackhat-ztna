b1f9d87e2efca30f55912287e4d7d186d47f5be2
Matt Schiller <mschiller@banyansecurity.io>
2022-08-09 08:30:36 -0700

    rdp target

--- 4-worker/worker.tf
+++ 4-worker/worker.tf
@@ -143,5 +143,6 @@ resource "aws_security_group_rule" "allow_rdp_worker" {
   from_port         = 3389
   to_port           = 3389
   protocol          = "tcp"
+  self = true
   security_group_id = aws_security_group.worker.id
 }
