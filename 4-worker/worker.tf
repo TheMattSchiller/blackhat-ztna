7a9a060a14a28b7008ff87d427bd9b4a4cb15281
Matt Schiller <mschiller@banyansecurity.io>
2022-08-08 17:38:18 -0700

    open 80

--- 4-worker/worker.tf
+++ 4-worker/worker.tf
@@ -102,10 +102,19 @@ resource "aws_security_group_rule" "allow_ssh_worker" {
   security_group_id = aws_security_group.worker.id
 }
 
-resource "aws_security_group_rule" "allow_web_worker" {
+resource "aws_security_group_rule" "allow_web_worker2" {
   type              = "ingress"
   from_port         = 8000
   to_port           = 8000
+  protocol          = "tcp"
+  cidr_blocks       = ["0.0.0.0/0"]
+  security_group_id = aws_security_group.worker.id
+}
+
+resource "aws_security_group_rule" "allow_web_worker" {
+  type              = "ingress"
+  from_port         = 80
+  to_port           = 80
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
   security_group_id = aws_security_group.worker.id
