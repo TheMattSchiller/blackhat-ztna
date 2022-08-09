0132044d984d9de59ec733fcd0f42e7b03e745f7
Matt Schiller <mschiller@banyansecurity.io>
2022-08-09 08:26:35 -0700

    rdp target

--- 4-worker/worker.tf
+++ 4-worker/worker.tf
@@ -134,6 +134,6 @@ resource "aws_security_group_rule" "allow_egress_worker" {
   from_port         = 0
   to_port           = 0
   protocol          = "-1"
-  cidr_blocks       = ["0.0.0.0/0"]
+  self = true
   security_group_id = aws_security_group.worker.id
 }
